8e502831b4860a19109ef47fbe337ec27a045750
SoundPJ <57010936@kmitl.ac.th>
2022-06-17 12:48:28 +0700

    Small changes

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: pjerddee <pjerddee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 08:26:54 by pjerddee          #+#    #+#             */
-/*   Updated: 2022/06/17 12:25:30 by pjerddee         ###   ########.fr       */
+/*   Updated: 2022/06/17 12:48:20 by pjerddee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 //Priority # > + > space
-char	set_flag1(char o_flag1, char n_flag1)
+static char	set_flag1(char o_flag1, char n_flag1)
 {
 	if (o_flag1 == '\0')
 		return n_flag1;
@@ -27,7 +27,7 @@ char	set_flag1(char o_flag1, char n_flag1)
 
 
 //Priority - > 0
-char	set_flag2(char o_flag2, char n_flag2)
+static char	set_flag2(char o_flag2, char n_flag2)
 {
 	if (o_flag2 == '\0')
 		return n_flag2;
@@ -61,7 +61,7 @@ t_cv	get_spcf(char *s)
 		s++;
 	}
 	ret.type = *s;
-	// printf("type = %c flag1 = %c flag2 = %c sharp = %d width = %d precision = %d len = %d\n", ret.type, ret.flag1, ret.flag2, ret.sharp, ret.width, ret.precision, ret.len);
+	printf("type = '%c' flag1 = '%c' flag2 = '%c' width = '%d' precision = '%d' len = '%d'\n", ret.type, ret.flag1, ret.flag2, ret.width, ret.precision, ret.len);
 	return (ret);
 }
 
@@ -94,12 +94,19 @@ int	ft_printf(const char *placeholders, ...)
 
 int	main(void)
 {
-	// printf("%d", ft_strlen("Hello"));
-	// ft_putchar('a');
-	// ft_putstr("Hello");
-	// printf(" ret:%d\n", ft_putnbr(-152483, 10, UPPER));
-	// printf("%+-2.4d", 9);
-	// void* a = "Hello";
-	// printf("%p", a);
+	// get_spcf("+5d");
+	// get_spcf("+-5d");
+	// get_spcf("-+5d");
+	// get_spcf("+0.4d");
+
+	// get_spcf("- 6d");
+	// get_spcf("0 6d");
+	// get_spcf("- 7.4d");
+	// get_spcf("- 5.4d");
+	// get_spcf("0 .5d");
+
+	// get_spcf("#04x");
+	// get_spcf("-#4x");
+	// get_spcf("#2.1x");
 	return (0);
 }
